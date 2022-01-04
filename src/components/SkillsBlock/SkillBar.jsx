1e073dc1fadbe2b78fe0fe3bec7b35da31bb433e
friedooo <friedlich3@gmail.com>
2022-01-05 01:33:43 +0300

    feat: working on svg

--- src/components/SkillsBlock/SkillBar.jsx
+++ src/components/SkillsBlock/SkillBar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { CombatSkills, CripplingStrikes } from "../../images/Combat";
+import {
+  CombatSkills,
+  FastAttack,
+  StrongAttack,
+  Defense,
+  Marksmanship,
+  BattleTrance,
+  MuscleMemory,
+  StrengthTraining,
+  ArrowDeflection,
+  LightningReflexes,
+  Resolve,
+  PreciseBlows,
+  CrushingBlows,
+  FleetFooted,
+  ColdBlood,
+  Undying,
+  Whirl,
+  Rend,
+  Counterattack,
+  AnatomicalKnowledge,
+  RazorFocus,
+  CripplingStrikes,
+} from "../../images/Combat";
 
 import {
   plusSkillPoint,
@@ -29,38 +52,126 @@ function SkillBar() {
       <button onClick={onMinusPoint}>-</button>
       <button onClick={onClearSkill}>clear</button> */}
       <div className="content">
-        <div className="test">
+        <div className="skill">
           <CombatSkills />
           <div className="counter">1</div>
         </div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
+        <div className="skill">
+          {" "}
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          {" "}
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          {" "}
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          {" "}
+          <div className="counter">1</div>
+        </div>
         <div className="skill-info">
           <span>Умения</span>
           <span>Очков потрачено: 1</span>
         </div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
+        <div className="skill">
+          <FastAttack />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <StrongAttack />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <Defense />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <Marksmanship />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <BattleTrance />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <MuscleMemory />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <StrengthTraining />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <ArrowDeflection />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <LightningReflexes />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <Resolve />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <PreciseBlows />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <CrushingBlows />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <FleetFooted />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <ColdBlood />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <Undying />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <Whirl />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <Rend />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <Counterattack />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <AnatomicalKnowledge />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <RazorFocus />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <CripplingStrikes />
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <div className="counter">1</div>
+        </div>
+        <div className="skill">
+          <div className="counter">1</div>
+        </div>
       </div>
     </div>
   );
