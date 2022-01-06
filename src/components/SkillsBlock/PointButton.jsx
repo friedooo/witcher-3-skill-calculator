a7249001d9748ee9c17d842d95d891479c8505ea
friedooo <friedlich3@gmail.com>
2022-01-06 19:33:03 +0300

    feat: start work on point btn

---
+++ src/components/SkillsBlock/PointButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function PointButton() {
+  return (
+    <div className="point-btn">
+      <div>+</div>
+      <div>Full</div>
+      <div>-</div>
+      <div>Reset</div>
+    </div>
+  );
+}
+
+export default PointButton;
