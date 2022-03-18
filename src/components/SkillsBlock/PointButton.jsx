8896961733f305d522095b35ca5e2f4433b52e18
friedooo <friedlich3@gmail.com>
2022-03-18 04:23:17 +0300

    feat: restyle point-btn

--- src/components/SkillsBlock/PointButton.jsx
+++ src/components/SkillsBlock/PointButton.jsx
@@ -3,10 +3,9 @@ import React from "react";
 function PointButton() {
   return (
     <div className="point-btn">
+      <div className="reset">Reset</div>
       <div className="add">+</div>
       <div className="remove">-</div>
-
-      <div className="remove">Reset</div>
     </div>
   );
 }
