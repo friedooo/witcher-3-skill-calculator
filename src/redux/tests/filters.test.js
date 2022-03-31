88b49f879fc768c4d26e35e3b14bb009eb41ebbd
friedooo <friedlich3@gmail.com>
2022-04-01 02:45:10 +0300

    fix: fix

---
+++ src/redux/tests/filters.test.js
@@ -0,0 +1,24 @@
+import filters from "../reducers/filters";
+
+const initialState = {
+  category: 0,
+  sortBy: "popular",
+};
+
+it("switch category to 2", () => {
+  let action = {
+    type: "SET_CATEGORY",
+    catIndex: 2,
+  };
+  let newState = filters(initialState, action);
+  expect(newState.category).toBe(2);
+});
+
+it("switch sortType to price", () => {
+  let action = {
+    type: "SET_SORT_BY",
+    sortType: "price",
+  };
+  let newState = filters(initialState, action);
+  expect(newState.sortBy).toBe("price");
+});
